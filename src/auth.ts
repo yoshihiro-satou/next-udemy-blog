b778a99db62c0b9a228edd37f2cad69bbb604974
yoshihiro satou <128808496+yoshihiro-satou@users.noreply.github.com>
2025-11-24 21:03:37 +0900

    auth.config

---
+++ src/auth.ts
@@ -0,0 +1,34 @@
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+import { prisma } from './lib/prisma'
+import bcrptjs from 'bcryptjs'
+
+async function getUser(email: string) { // ユーザー取得関数
+  return await prisma.user.findUnique({
+    where: { email: email }
+  })
+}
+
+export const { auth, signIn, signOut, handlers } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(8) })
+          .safeParse(credentials);
+
+          if(parsedCredentials.success) {
+            const { email, password } = parsedCredentials.data;
+            const user = await getUser(email); // ユーザー取得
+            if(!user) return null;
+            const passwordsMatch = await bcrptjs.compare(password, user.password);
+            if(passwordsMatch) return user;
+          }
+          return null;
+      },
+    }),
+  ],
+});
